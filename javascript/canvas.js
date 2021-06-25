4445011521eb3198c88bafe238ed1d0b128fcc1e
clause <claussdev5@gmail.com>
2021-06-25 19:51:20 -0400

    all done

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -1,34 +1,142 @@
 class HangmanCanvas {
   constructor(secretWord) {
     this.context = document.getElementById('hangman').getContext('2d');
-    // ... your code goes here
+    this.secretWord = secretWord;
   }
 
   createBoard() {
-    // ... your code goes here
+    console.log('creating board')
+    this.context.clearRect(0,0,1200, 800);
+    this.drawLines();
   }
 
   drawLines() {
-    // ... your code goes here
+    let start = 250;
+    for(let i =0; i<this.secretWord.length ; i++){
+      this.context.beginPath();
+      this.context.moveTo(start,700);
+        this.context.lineTo(start +30, 700);
+        this.context.stroke();
+        start +=50;
+        this.context.closePath();
+    }
   }
 
-  writeCorrectLetter(index) {
-    // ... your code goes here
+  writeCorrectLetter(letter) {
+    for(let i =0 ; i < this.secretWord.length ; i++){
+          if(letter === this.secretWord.charAt(i)){
+            this.context.fillStyle ='green';
+            this.context.font = '42px Kirang Haerang, cursive';
+          this.context.fillText(`${letter}`, 255 + (50 * i) , 695 , 20);
+          }
+    }
+    
   }
 
   writeWrongLetter(letter, errorsLeft) {
-    // ... your code goes here
+    if(errorsLeft  >= 0 ){this.context.clearRect(450,0,hangman.width, 300);
+    this.context.fillStyle ='crimson';
+    this.context.font = '42px Staatliches, cursive';
+    this.context.fillText(`Wrong Letters:`, 750, 150);
+
+    
+    this.context.font = '40px Kirang Haerang, cursive';
+      console.log(errorsLeft)
+      this.context.fillText(`${letter}`, 600 + (50 * (10 - errorsLeft)), 200,20);
+      this.context.stroke();
+      this.drawHangman(10 - errorsLeft);}
   }
 
   drawHangman(errorsLeft) {
-    // ... your code goes here
+    switch(errorsLeft){
+      case 1:
+        this.context.beginPath();
+        this.context.moveTo(0,700);
+        this.context.lineTo(200,700);
+        this.context.lineTo(100,650);
+        this.context.lineTo(0,700);
+        this.context.stroke();
+        this.context.closePath();
+        break;
+
+      case 2:
+        this.context.beginPath();
+        this.context.moveTo(100,650);
+        this.context.lineTo(100,250);
+        this.context.stroke();
+        this.context.closePath();
+        break;
+
+      case 3:
+        this.context.beginPath();
+        this.context.moveTo(100,250);
+        this.context.lineTo(300,250);
+        this.context.stroke();
+        this.context.closePath();
+
+        break;
+
+      case 4:
+        this.context.beginPath();
+        this.context.moveTo(300,250);
+        this.context.lineTo(300,300);
+        this.context.stroke();
+        this.context.closePath();
+        break;
+
+      case 5:
+        this.context.beginPath();
+        this.context.arc(300,330,30,0, Math.PI * 2);
+        this.context.strokeStyle= 'black';
+        this.context.stroke();
+        this.context.closePath();
+        break;
+      case 6:
+        this.context.beginPath();
+        this.context.moveTo(300,360);
+        this.context.lineTo(300,480);
+        this.context.stroke();
+        this.context.closePath();
+        break;
+      case 7:
+        this.context.beginPath();
+        this.context.moveTo(300,380);
+        this.context.lineTo(250,450);
+        this.context.stroke();
+        this.context.closePath();
+        break;
+      case 8:
+        this.context.beginPath();
+        this.context.moveTo(300,380);
+        this.context.lineTo(350,450);
+        this.context.stroke();
+        this.context.closePath();
+        break;
+      case 9:
+        this.context.beginPath();
+        this.context.moveTo(300,480);
+        this.context.lineTo(250,550);
+        this.context.stroke();
+        this.context.closePath();
+        break;
+      case 10:
+        this.context.beginPath();
+        this.context.moveTo(300,480);
+        this.context.lineTo(350,550);
+        this.context.stroke();
+        this.context.closePath();
+        this.gameOver();
+        break;
+    }
   }
 
   gameOver() {
-    // ... your code goes here
+    this.context.drawImage(gameOverImg,550,250,500, 350);
+    this.context.font = '42px Staatliches, cursive';
+    this.context.fillText(`Secret Word: ${hangman.secretWord}`, 650, 650);
   }
 
   winner() {
-    // ... your code goes here
+    this.context.drawImage(awesomeImg,350,250,600, 400);
   }
 }
